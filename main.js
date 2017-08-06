0b73192ee123c50fab32681c057bb5b791b08f3e
kevin578 <kbriggs@aapl000894.local>
2017-08-06 12:00:09 -0400

    functional

--- main.js
+++ main.js
@@ -3,22 +3,52 @@ $(document).ready(function(){
 var channels = ['freecodecamp', 'ESL_SC2', 'EJ_SA', 'timmac'];
  
 
-for (chan in channels) {
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+ channels[chan] + '?callback=?', function(data) { 	
+channels.forEach(function(channel){
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+ channel + '?callback=?', function(data) { 	
+		render(data);
+});
+
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+ channel + '?callback=?', function(data) { 	
+	var id = channel.toLowerCase();
+
+	isStreaming(id)
+});
+	});
 
 
-  	var template = $('#template').html();
+function render(data){
+
+	var template = $('#template').html();
   	Mustache.parse(template);
+  	
   	var rendered = Mustache.render(template, {
   	logo: data.logo,
   	name: data.display_name,
   	link: data.url,
   	status: data.status,
-  	banner: data.profile_banner
+  	banner: data.profile_banner,
+  	id: data.name
   });
   
   $('#target').append(rendered);
-});
+
+}
+
+function isStreaming(id) {
+
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+ id + '?callback=?', function(data) { 
+	console.log(id)
+	if (data.stream != null) {
+		$('#' + id).addClass('online');
+		$('#' + id + ' .status').html("Online")
+
+	}
+	else {
+		$('#' + id).addClass('offline');
+		$('#' + id + ' .status').html("Offline")
+	}
+
+	});
 }
 
   
