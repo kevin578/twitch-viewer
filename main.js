7ee7a0b80595a263caaff5dd11b6f96accfe6f9b
kevin578 <kbriggs@aapl000894.local>
2017-08-06 07:55:47 -0400

    display just about finished

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 $(document).ready(function(){
 
-var channels = ['freecodecamp', 'ESL_SC2', 'EJ_SA'];
+var channels = ['freecodecamp', 'ESL_SC2', 'EJ_SA', 'timmac'];
  
-console.log(channels)
 
 for (chan in channels) {
-	$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+ channels[chan] + '?callback=?', function(data) {
-  	console.log(data);
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/channels/'+ channels[chan] + '?callback=?', function(data) { 	
 
-  var template = $('#template').html();
-  Mustache.parse(template);
-  var rendered = Mustache.render(template, {
-  	logo: data.logo
+
+  	var template = $('#template').html();
+  	Mustache.parse(template);
+  	var rendered = Mustache.render(template, {
+  	logo: data.logo,
+  	name: data.display_name,
+  	link: data.url,
+  	status: data.status,
+  	banner: data.profile_banner
   });
   
   $('#target').append(rendered);
-
 });
 }
 
